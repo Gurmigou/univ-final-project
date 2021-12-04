f47a463e649ab667dfeda63791d1f9082d2beb30
Fulgur3 <74140068+Fulgur3@users.noreply.github.com>
2021-12-05 00:00:54 +0200

    Add files via upload
    added repo files

---
+++ back-end/src/main/java/com/infpulse/studentspoll/repository/PossibleAnswerRepository.java
@@ -0,0 +1,34 @@
+package com.infpulse.studentspoll.repository;
+
+import com.infpulse.studentspoll.model.entity.PossibleAnswer;
+import com.infpulse.studentspoll.model.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PossibleAnswerRepository extends JpaRepository<PossibleAnswer, Long> {
+
+    //Should return all possible answers that belong to question
+    List<PossibleAnswer> findAllByQuestion(Question question);
+
+    @Query("SELECT pa FROM PossibleAnswer pa " +
+            "WHERE pa.id = :id AND pa.possibleAnswer = :possibleAnswer")
+    Optional<PossibleAnswer> findAnswerByQuestionIdAndPossibleAnswer(@Param("id") Long id,
+                                                     @Param("possibleAnswer") String possibleAnswer);
+
+    @Query("SELECT pa FROM PossibleAnswer pa " +
+            "WHERE pa.question.id = :questionId AND pa.id = :answerId")
+    PossibleAnswer findPossibleAnswerByQuestionIdAndAnswerId(@Param("questionId") long questionId,
+                                                             @Param("answerId") long answerId);
+
+    //should find id's of possible answers by question id and possibleAnswer
+    @Query("SELECT pa.id FROM PossibleAnswer pa " +
+            "WHERE pa.id = :qid AND pa.possibleAnswer IN :pNames")
+    List<Long> findPossibleAnswersIdsByQuestionIdAndByPossibleAnswers(@Param("qid") long questionId,
+                                                                      @Param("pNames") List<String> possibleAnswersNames);
+}
