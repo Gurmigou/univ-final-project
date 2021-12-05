d89618f12b71d58b20a0dd3307c0235ded624098
Fulgur3 <74140068+Fulgur3@users.noreply.github.com>
2021-12-05 17:40:35 +0200

    added navbar footer

---
+++ front-end/src/navbarFooter/NavItems.ts
@@ -0,0 +1,17 @@
+export const NavItems: { title: string, url: string, cName: string }[]  = [
+    {
+        title: 'Create form',
+        url: '/form/construct',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Form list',
+        url: '/user/form-list',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Account',
+        url: '/user/account',
+        cName: 'nav-links'
+    }
+]
